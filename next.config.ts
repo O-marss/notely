e5b5c778f91cabb0a5a9b152c3019a214299993f
0mar-cc <omarmoh3010@gmail.com>
2025-04-15 23:18:49 +0200

    modified workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-const isGithubPages = process.env.NODE_ENV === 'production'
+const isGithubPages = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isGithubPages ? '/notely' : '',
-  assetPrefix: isGithubPages ? '/notely/' : '',
+  basePath: isGithubPages ? "/notely" : "",
+  assetPrefix: isGithubPages ? "/notely/" : "",
   trailingSlash: true,
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
